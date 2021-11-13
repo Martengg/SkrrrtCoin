a08765859e243b9cabc5fc32549eac946b1f8b69
diesdasnicolas <80157943+diesdasnicolas@users.noreply.github.com>
2021-11-13 15:38:59 +0100

    added the mining-script in python

--- src/server/js/mining/utils/userInterface.js
+++ src/server/js/mining/utils/userInterface.js
@@ -1,3 +1,5 @@
+// later there comes a real interface not a key-cache
+
 export function startInterface() {
     return "d10e859f-2853-487d-a45c-15bcf191dd1d";
 }
---
+++ src/server/python/mining/mine.py
@@ -0,0 +1,63 @@
+from requests import get, post
+from hashlib import md5
+from sys import maxsize
+
+
+API_ENDPOINT = "https://skrt.koaladev.de"
+WALLET_KEY = "193"
+
+START_MINING_PATH = "/api/mine/start/skrt"
+FINISH_MINING_PATH = "/api/mine/finish/skrt"
+
+
+def fetch_data():
+    try:
+        response = get(API_ENDPOINT + START_MINING_PATH)
+        return response.json()["uuid"], response.json()["nonce"]
+
+    except:
+        return False, False
+
+
+def crack_hash(nonce):
+    solution = 0
+
+    while True:
+        attempt = "{}".format(md5(str(nonce + solution).encode()).hexdigest(), '02x')
+
+        if attempt.startswith("000000"): # check if the cracking is finished
+            break
+
+        solution += 1
+
+        if solution == maxsize: # check if the maxsize is reached
+            break
+    
+    return solution
+
+
+def post_data(uuid, solution):
+    # prepare for posting
+    url = API_ENDPOINT + FINISH_MINING_PATH
+    posting_data = {"uuid": uuid, "solution": solution, "walletKey": WALLET_KEY}
+
+    # post the data
+    post(url, data=posting_data)
+    return
+
+
+def main():
+    while True:
+        # fetch the data
+        uuid, nonce = fetch_data()
+
+        if uuid and nonce: # check if the data has been fetched successfully
+            # crack the solution
+            solution = crack_hash(nonce)
+
+            # post the data
+            post_data(uuid, solution)
+
+
+if __name__ == "__main__":
+    main()
